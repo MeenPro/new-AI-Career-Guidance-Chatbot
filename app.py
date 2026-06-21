46359ac949e9ae6d52c14687bb78f7855b2df8d0
MeenPro <princekumar828782@gmail.com>
2026-06-21 18:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 # Gemini API Configuration
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 # Title
 st.title("AI Career Guidance Chatbot")
 
